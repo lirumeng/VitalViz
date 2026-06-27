ff9c5f4544162d30a69ec3bcc5b58c7afe1143d5
lirumeng <lirumeng@nsoc.cn>
2026-06-27 17:50:46 +0800

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "github" ? "/VitalViz/" : "/",
   plugins: [vue()]
-});
+}));
